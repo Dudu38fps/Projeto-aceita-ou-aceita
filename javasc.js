dc590b5ba80f550f386b912d17685318a9c9a6e7
Eduardo Feliciano <138251065+Dudu38fps@users.noreply.github.com>
2024-09-11 14:58:21 -0300

    updates

--- javasc.js
+++ javasc.js
@@ -1,17 +1,21 @@
 
-        document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     const noButton = document.getElementById("botao-negativo");
 
-    noButton.addEventListener("mouseover", function () {
+    function moveButton() {
         const x = Math.floor(Math.random() * (window.innerWidth - noButton.clientWidth));
         const y = Math.floor(Math.random() * (window.innerHeight - noButton.clientHeight));
         noButton.style.position = "absolute";
         noButton.style.left = `${x}px`;
         noButton.style.top = `${y}px`;
-    });
+    }
+
+    noButton.addEventListener("mouseover", moveButton);
+    noButton.addEventListener("click", moveButton);
+
 });
 
 function eventoDeClique() {
-    window.location.href = "https://youtu.be/uYHNdTPV7pM?si=960l-4nbZwW3ln1J";
+    window.open("https://youtu.be/uYHNdTPV7pM?si=960l-4nbZwW3ln1J", '_blank');
     
 }
